6a434bf8b163cd831c8859ae5f46c0a113f41650
asdfg <asdfghjkl38939@gmail.com>
2023-10-27 21:34:10 +0300

    Добавьте файлы проекта.

---
+++ Practice7/Comments.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice7
+{
+    internal class Comments
+    {
+        private List<string> comments;
+
+        /// <summary>
+        /// Используется коллекция List<string> для хранения комментариев. 
+        /// Это было выбрано из-за своей простоты и эффективности для этой задачи. 
+        /// Она позволяет легко добавлять, удалять и получать комментарии.
+        /// </summary>
+
+        /// <summary>
+        /// Creating of empty list
+        /// </summary>
+        public Comments()
+        {
+            comments = new List<string>();
+        }
+
+        /// <summary>
+        /// Adding of comment in List of comments 
+        /// </summary>
+        public void AddComment(string comment)
+        {
+            comments.Add(comment);
+        }
+
+        /// <summary>
+        /// Removing of comment from List of comments 
+        /// </summary>
+        public void RemoveComment(int index)
+        {
+            comments.RemoveAt(index - 1);
+        }
+
+        /// <summary>
+        /// Getting List of comments 
+        /// </summary>
+
+        public List<string> GetAllComments()
+        {
+            return comments;
+        }
+
+        public override string ToString()
+        {
+            string allComments = "";
+            int i = 1;
+            if (GetAllComments().Count == 0)
+            {
+                allComments += "Пока нет комментариев";
+            } else
+            {
+                foreach (string comment in GetAllComments())
+                {
+                    allComments += $"{i}. " + comment + "\n";
+                    i++;
+                }
+            }
+            return allComments;
+        }
+    }
+}
---
+++ Practice7/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Practice7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comments comments = new Comments();
+            Console.WriteLine(comments);
+            int act;
+
+            bool correct = false;
+            while (!correct)
+            {
+                Console.WriteLine("Если хотите добавить комментарий введите 1, чтобы удалить введите 2, чтобы выйти из приложения введите 3");
+                try
+                {
+                    act = Convert.ToInt32(Console.ReadLine());
+                    if (act == 1) 
+                    {
+                        Console.WriteLine("Введите комментарий, который хотите добавить");
+                        string comment = Console.ReadLine();
+                        if (comment is not null && comment.Length != 0)
+                        {
+                            comments.AddComment(comment);
+                            Console.WriteLine(comments);
+                        } else
+                        {
+                            Console.WriteLine("Вы не можете добавлять пустой комментарий");
+                        }
+
+                    } else if (act == 2)
+                    {
+                        if (comments.GetAllComments().Count == 0)
+                        {
+                            Console.WriteLine("Нет комментариев, которые можна удалить");
+                        } else
+                        {
+                            bool correctIndex = false;
+                            while (!correctIndex)
+                            {
+                                Console.WriteLine("Введите номер комментария, который хотите удалить");
+                                try
+                                {
+                                    int index = Convert.ToInt32(Console.ReadLine());
+                                    if (index <= 0 && index > comments.GetAllComments().Count)
+                                    {
+                                        Console.WriteLine("Что-то пошло не так");
+                                        correctIndex = false;
+                                    }
+                                    else
+                                    {
+                                        comments.RemoveComment(index);
+                                        Console.WriteLine(comments);
+                                        correctIndex = true;
+                                    }
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("Неверный формат");
+                                    correctIndex = false;
+                                }
+                            }
+                        }
+                    } else if (act == 3)
+                    {
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("Были указаны неверные цифры");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Что-то не так");
+                    correct = false;
+                }
+            }
+        }
+    }
+}
